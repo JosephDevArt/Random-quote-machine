747beeaed3beb503b53bd00e7230e33ec038674d
JosephDevArt <jbdevart@gmail.com>
2020-02-29 20:44:19 +0300

    refractored css styles (quote-text)

--- src/components/QuoteText.jsx
+++ src/components/QuoteText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function QuoteText(props) {
   return (
-    <p style={{ color: props.color }} id="text" className="quote_text">
+    <p style={{ color: props.color }} id="text" className="quote-text">
       {props.quote}
     </p>
   );
